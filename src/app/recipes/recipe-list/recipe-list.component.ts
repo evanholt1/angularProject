543c867e3605db9e288d449e4179ad96e267725c
evan holt <adiga_knight@hotmail.com>
2020-02-02 20:06:24 +0200

    after chapter 6

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output,AfterViewInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -6,15 +6,26 @@ import { Recipe } from '../recipe.model';
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, AfterViewInit {
   recipes : Recipe[] = [
     new Recipe("recipe 1","first recipe","https://get.pxhere.com/photo/dish-food-cuisine-ingredient-produce-recipe-meat-vegetarian-food-comfort-food-side-dish-bento-Yong-tau-foo-meal-salad-teriyaki-chinese-food-japanese-cuisine-donburi-1575227.jpg"),
     new Recipe("recipe 2","second recipe","https://get.pxhere.com/photo/dish-meal-food-salad-vegetable-recipe-healthy-snack-cuisine-delicious-pasta-cheese-noodle-cook-grapefruit-asian-food-noodles-vegetarian-zucchini-spaghetti-vegetarian-food-diet-mozzarella-thai-food-italian-food-leaf-vegetable-european-food-southeast-asian-food-capellini-taglierini-1375599.jpg")
   ];
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
+  // onRecipeClicked(clickedRecipeIndex : number) {
+  //   this.recipeItemClicked.emit(this.recipes[clickedRecipeIndex]);
+  // }
+  onRecipeSelected(recipe : Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+   // this.onRecipeClicked(0);
+  }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -7,7 +7,14 @@ import { Ingredient } from '../shared/Ingredient';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients :Ingredient[] = [new Ingredient("Apple",5)];
+  ingredients :Ingredient[] = [
+    new Ingredient("Apple",5),
+    new Ingredient("Tomato",10)
+  ];
+
+  onIngredientAdded(ingredient : Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 
   constructor() { }
 
